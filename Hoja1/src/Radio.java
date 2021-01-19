7d294d35ccc4a5e4ee576050613f6c1a8251ae87
Priscigs <39099667+Priscigs@users.noreply.github.com>
2021-01-19 14:50:33 -0600

    Prueba1

---
+++ Hoja1/src/Radio.java
@@ -0,0 +1,23 @@
+
+public class Radio implements Interface{
+	
+	public boolean encenderApagar() {
+		return true;
+	}
+	
+	public boolean amFm() {
+		return true;
+	}
+	
+	public String guardar(int btn) {
+		return "0";
+	}
+	
+	public String seleccionar(int btn) {
+		return "0";
+	}
+	
+	public double avanzar() {
+		return 0;
+	}
+}
