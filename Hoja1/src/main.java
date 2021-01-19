114112c1083ad267b2e66a567bfa0ee5673b01ca
guzmanalejandra <77361246+guzmanalejandra@users.noreply.github.com>
2021-01-19 14:59:18 -0600

    Create main.java

---
+++ Hoja1/src/main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main{
+    public void display_menu(){
+        System.out.println("Radio: \n)(1. Encender Radio\n");
+        System.out.println("Selecciona una opcion: ")
+
+    }
+
+    public main(){
+        Scanner in = New Scanner (System.in);
+
+        display_menu();
+        switch(in.nextInt() ) {
+            case 1: 
+        }
+
+    }
+}
